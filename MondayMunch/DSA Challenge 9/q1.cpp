acd308ba3047ed6d17e584dab847b90cb89507e6
Ojas Srivastava <srivastavaojas454@gmail.com>
2025-10-27 17:43:51 +0530

    add VSCode configuration files and initial C++ source code

---
+++ MondayMunch/DSA Challenge 9/q1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <string>
+#include <cmath>
+#include <limits>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    return 0;
+}
